a1a04ad69fa6ec07283c470d9ddcc80a09eb85ef
jass-ubuntu-dev-14-f <jifill25@gmail.com>
2016-11-28 14:38:24 -0800

    update: added c-code for subtraction and multiply; (subtraction and multiply) need testing, and (I'm) having trouble linking binops.ll at the moment

--- lib/binopts.cpp
+++ lib/binopts.cpp
@@ -6,3 +6,21 @@ int rpiLop_add(int a, int b) {
     }
     return a;
 }
+int rpiLop_subtract(int a, int b)
+{
+  b = -b;
+  while (b!=0)
+    {
+      int carry = (a & b);
+      a = a ^ b;
+      b = carry << 1;
+    }
+  return a;
+
+}
+int rpiLop_multiply(int a, int b)
+{
+  int total = 0;
+  for(;b;total+=rpiLop_add(total,a) ,b--);
+  return total;
+}
