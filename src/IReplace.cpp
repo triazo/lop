0c508b40c0deda60f6f8df8692c464b499c7e7bb
adam <adam@triazo.net>
2016-11-30 20:29:09 -0500

    Replace everything except signed division

--- src/IReplace.cpp
+++ src/IReplace.cpp
@@ -34,8 +34,8 @@ namespace{
                 auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
                                            dualint,
                                            false);
-                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_subtract", t));
-                auto ci = CallInst::Create(t, addFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                auto subFunc = cast<Function>(M->getOrInsertFunction("rpiLop_subtract", t));
+                auto ci = CallInst::Create(t, subFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::Mul) {
@@ -44,8 +44,8 @@ namespace{
                 auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
                                            dualint,
                                            false);
-                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_multiply", t));
-                auto ci = CallInst::Create(t, addFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                auto mulFunc = cast<Function>(M->getOrInsertFunction("rpiLop_multiply", t));
+                auto ci = CallInst::Create(t, mulFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::UDiv) {
@@ -54,8 +54,48 @@ namespace{
                 auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
                                            dualint,
                                            false);
-                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_divide", t));
+                auto divFunc = cast<Function>(M->getOrInsertFunction("rpiLop_divide", t));
+                auto ci = CallInst::Create(t, divFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                ReplaceInstWithInst(&I, ci);
+            }
+            else if (I.getOpcode() == Instruction::FAdd) {
+                auto M = I.getParent()->getParent()->getParent();
+                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
+                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                                           dualfloat,
+                                           false);
+                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fadd", t));
                 auto ci = CallInst::Create(t, addFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                ReplaceInstWithInst(&I, ci);
+            }
+            else if (I.getOpcode() == Instruction::FSub) {
+                auto M = I.getParent()->getParent()->getParent();
+                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
+                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                                           dualfloat,
+                                           false);
+                auto subFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fsub", t));
+                auto ci = CallInst::Create(t, subFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                ReplaceInstWithInst(&I, ci);
+            }
+            else if (I.getOpcode() == Instruction::FMul) {
+                auto M = I.getParent()->getParent()->getParent();
+                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
+                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                                           dualfloat,
+                                           false);
+                auto mulFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fmul", t));
+                auto ci = CallInst::Create(t, mulFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                ReplaceInstWithInst(&I, ci);
+            }
+            else if (I.getOpcode() == Instruction::FDiv) {
+                auto M = I.getParent()->getParent()->getParent();
+                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
+                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                                           dualfloat,
+                                           false);
+                auto divFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fdiv", t));
+                auto ci = CallInst::Create(t, mulFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
         }
