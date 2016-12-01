fac97ed56ca8e8609db614c0579b9f0a69db0bef
adam <adam@triazo.net>
2016-12-01 12:20:22 -0500

    Apparently I changed a lot of stuff last night and I'm not too sure what

--- lib/binopts.cpp
+++ lib/binopts.cpp
@@ -8,140 +8,243 @@
 #define max_float 0xffffffff
 
 extern "C" {
-int rpiLop_add(int a, int b) {
-    while (b) {
-        int carry = (a & b);
-        a = a ^ b;
-        b = carry << 1;
+    int rpiLop_add(int a, int b) {
+        while (b) {
+            int carry = (a & b);
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
     }
-    return a;
-}
+
+    long long int rpiLop_add64(long long int a, long long int b) {
+        while (b) {
+            int carry = (a & b);
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
 
 
-int rpiLop_subtract(int a, int b)
-{
-  b = ~b + 1;
-  while (b)//formerly b!=0
+    int rpiLop_sub(int a, int b)
     {
-      int carry = (a & b);
-      a = a ^ b;
-      b = carry << 1;
+        b = ~b + 1;
+        while (b)//formerly b!=0
+        {
+            int carry = (a & b);
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
     }
-  return a;
 
-}
-int rpiLop_multiply(int a, int b)
-{
-  int total = 0;
-  //for(;b;total+=rpiLop_add(total,a) ,b--);
-  for(;b;total+=a,b--);
-  return total;
-}
-
-unsigned int rpiLop_divide(unsigned int dividend, unsigned int divisor)
-{
-  unsigned int quotient,remainder,t;
-  int bits_left = 8 * sizeof(unsigned int);
-  quotient = dividend;
-  remainder = 0;
-  //for(;bits_left;t=quotient,quotient = rpiLop_add(quotient,quotient); remainder = rpiLop_add(rpiLop_add(remainder,remainder),(quotient<t)),((remainder >= divisor)?remainder = rpiLop_subtract(rem,divisor),quotient= rpiLop_add(quotient,1):quotient=quotient),bits_left--);
-  for(t=quotient;bits_left;t=quotient,bits_left--)
+    long long int rpiLop_sub64(long long int a, long long int b)
     {
-      quotient = rpiLop_add(quotient,quotient); //add
-      remainder = rpiLop_add(rpiLop_add(remainder,remainder),(quotient<t));//nested add
-      (remainder >= divisor)?remainder = rpiLop_subtract(remainder,divisor),quotient= rpiLop_add(quotient,1):quotient=quotient;//conditional subtract and add
+        b = ~b + 1;
+        while (b)//formerly b!=0
+        {
+            int carry = (a & b);
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    int rpiLop_mul(int a, int b)
+    {
+        int total = 0;
+        //for(;b;total+=rpiLop_add(total,a) ,b--);
+        for(;b;total+=a,b--);
+        return total;
+    }
+
+    long long int rpiLop_mul64(long long int a, long long int b)
+    {
+        int total = 0;
+        //for(;b;total+=rpiLop_add(total,a) ,b--);
+        for(;b;total+=a,b--);
+        return total;
+    }
+
+    unsigned int rpiLop_div(unsigned int dividend, unsigned int divisor)
+    {
+        unsigned int quotient,remainder,t;
+        int bits_left = 8 * sizeof(unsigned int);
+        quotient = dividend;
+        remainder = 0;
+        //for(;bits_left;t=quotient,quotient = rpiLop_add(quotient,quotient); remainder = rpiLop_add(rpiLop_add(remainder,remainder),(quotient<t)),((remainder >= divisor)?remainder = rpiLop_subtract(rem,divisor),quotient= rpiLop_add(quotient,1):quotient=quotient),bits_left--);
+        for(t=quotient;bits_left;t=quotient,bits_left--)
+        {
+            quotient = rpiLop_add(quotient,quotient); //add
+            remainder = rpiLop_add(rpiLop_add(remainder,remainder),(quotient<t));//nested add
+            (remainder >= divisor)?remainder = rpiLop_sub(remainder,divisor),quotient= rpiLop_add(quotient,1):quotient=quotient;//conditional subtract and add
+        }
+
+
+        return quotient;
+
+    }
+
+    unsigned long long int rpiLop_div64(unsigned long long int dividend, unsigned long long int divisor)
+    {
+        unsigned long long int quotient,remainder,t;
+        long long int bits_left = 8 * sizeof(unsigned long long int);
+        quotient = dividend;
+        remainder = 0;
+        //for(;bits_left;t=quotient,quotient = rpiLop_add(quotient,quotient); remainder = rpiLop_add(rpiLop_add(remainder,remainder),(quotient<t)),((remainder >= divisor)?remainder = rpiLop_subtract(rem,divisor),quotient= rpiLop_add(quotient,1):quotient=quotient),bits_left--);
+        for(t=quotient;bits_left;t=quotient,bits_left--)
+        {
+            quotient = rpiLop_add(quotient,quotient); //add
+            remainder = rpiLop_add(rpiLop_add(remainder,remainder),(quotient<t));//nested add
+            (remainder >= divisor)?remainder = rpiLop_sub(remainder,divisor),quotient= rpiLop_add(quotient,1):quotient=quotient;//conditional subtract and add
+        }
+
+
+        return quotient;
+
     }
 
 
-  return quotient;
-
-}
-
-
-typedef union fp_bit_twiddler {
-    float f;
-    uint32_t i;
-} UNSAFEFPTHING;
+    typedef union fp_bit_twiddler {
+        float f;
+        uint32_t i;
+    } UNSAFEFPTHING;
 
 
-uint32_t rpiLop_fpadd2(uint32_t x, uint32_t y){
-    uint32_t result_mantissa;
-    uint32_t result_exponent;
-    uint32_t result_sign;
+    uint32_t rpiLop_fpadd2(uint32_t x, uint32_t y){
+        uint32_t result_mantissa;
+        uint32_t result_exponent;
+        uint32_t result_sign;
 
-    uint32_t different_sign = sign(x) ^ sign(y);
+        uint32_t different_sign = sign(x) ^ sign(y);
 
-    // catch NaN
-    if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
-    if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
+        // catch NaN
+        if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
+        if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
 
-    // catch Inf
-    if (!(exponent(x) ^ 0xFF) && !(exponent(y) ^ 0xFF)) {
-        // both are inf
-        if (different_sign)
-            // Inf - Inf
-            return 0x7F800000 + 1; // NaN
-        else
-            // both Inf or -Inf
-            return x;
-    }
-    else if (!(exponent(x) ^ 0xFF)) return x;
-    else if (!(exponent(y) ^ 0xFF)) return y;
+        // catch Inf
+        if (!(exponent(x) ^ 0xFF) && !(exponent(y) ^ 0xFF)) {
+            // both are inf
+            if (different_sign)
+                // Inf - Inf
+                return 0x7F800000 + 1; // NaN
+            else
+                // both Inf or -Inf
+                return x;
+        }
+        else if (!(exponent(x) ^ 0xFF)) return x;
+        else if (!(exponent(y) ^ 0xFF)) return y;
 
-    // both numbers are non-special
-    uint32_t exp_difference;
-    if (different_sign) {
-        exp_difference = exponent(y) + exponent(x);
-    }
-    else {
-        // no need to account for constant BO
-        // beware of underflow
-        if (exponent(x) > exponent(y)) exp_difference = exponent(x) - exponent(y);
-        else exp_difference = exponent(y) - exponent(x);
-    }
-
-
-    bool x_bigger_abs;
-    if      (exponent(x) > exponent(y)) x_bigger_abs = true;
-    else if (exponent(x) < exponent(y)) x_bigger_abs = false;
-    else if (mantissa(x) > mantissa(y)) x_bigger_abs = true;
-    else                                x_bigger_abs = false;
-
-    if (!different_sign) {
-        //both numbers have same sign (this is an add)
-        result_sign = sign(x);
-
-        if (x_bigger_abs) {
-            result_mantissa = (mantissa(x) << 1) + (mantissa(y) << 1) >> exp_difference;
-            result_exponent = exponent(x);
+        // both numbers are non-special
+        uint32_t exp_difference;
+        if (different_sign) {
+            exp_difference = exponent(y) + exponent(x);
         }
         else {
-            result_mantissa = (mantissa(y) << 1) + ((mantissa(x) << 1) >> exp_difference);
-            result_exponent = exponent(y);
+            // no need to account for constant BO
+            // beware of underflow
+            if (exponent(x) > exponent(y)) exp_difference = exponent(x) - exponent(y);
+            else exp_difference = exponent(y) - exponent(x);
         }
-        if (result_mantissa << 31) result_mantissa = (result_mantissa >> 1) + 1;
-        else result_mantissa = (result_mantissa >> 1);
-    }
-    else {
-        // this actually is a subtraction
 
-        if (x_bigger_abs) {
+
+        bool x_bigger_abs;
+        if      (exponent(x) > exponent(y)) x_bigger_abs = true;
+        else if (exponent(x) < exponent(y)) x_bigger_abs = false;
+        else if (mantissa(x) > mantissa(y)) x_bigger_abs = true;
+        else                                x_bigger_abs = false;
+
+        if (!different_sign) {
+            //both numbers have same sign (this is an add)
             result_sign = sign(x);
-            result_exponent = exponent(x);
 
-            // subtract and round to 23 bit
-            // this means making room in our 32bit representation
-            result_mantissa = (mantissa(x) << 1) - ((mantissa(y) << 1) >> exp_difference );
+            if (x_bigger_abs) {
+                result_mantissa = (mantissa(x) << 1) + (mantissa(y) << 1) >> exp_difference;
+                result_exponent = exponent(x);
+            }
+            else {
+                result_mantissa = (mantissa(y) << 1) + ((mantissa(x) << 1) >> exp_difference);
+                result_exponent = exponent(y);
+            }
+            if (result_mantissa << 31) result_mantissa = (result_mantissa >> 1) + 1;
+            else result_mantissa = (result_mantissa >> 1);
         }
         else {
-            result_sign = sign(y);
-            result_exponent = exponent(y);
+            // this actually is a subtraction
 
-            // subtract and round to 23 bit
-            // this means making room in our 32bit representation
-            result_mantissa = (mantissa(y) << 1) - ((mantissa(x) << 1) >> exp_difference);
+            if (x_bigger_abs) {
+                result_sign = sign(x);
+                result_exponent = exponent(x);
+
+                // subtract and round to 23 bit
+                // this means making room in our 32bit representation
+                result_mantissa = (mantissa(x) << 1) - ((mantissa(y) << 1) >> exp_difference );
+            }
+            else {
+                result_sign = sign(y);
+                result_exponent = exponent(y);
+
+                // subtract and round to 23 bit
+                // this means making room in our 32bit representation
+                result_mantissa = (mantissa(y) << 1) - ((mantissa(x) << 1) >> exp_difference);
+            }
+
+            if (result_mantissa << 31)  result_mantissa = ((result_mantissa >> 1) + 1);
+            else result_mantissa = (result_mantissa >> 1);
+
+            // normalize mantissa
+            uint32_t temp = result_mantissa << 9;
+            for (uint32_t count = 0; count < 23; ++count) {
+                if (!((temp << count) >> 31)) {
+                    result_mantissa <<= ++count; // leading 1, so shift 1 more time
+                    result_exponent -= count;
+                    break;
+                }
+            }
         }
+        uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
+        return result;
+    }
 
+    float rpiLop_fpadd(float x, float y){
+        UNSAFEFPTHING a, b, result;
+        a.f = x;
+        b.f = y;
+
+        result.i = rpiLop_fpadd2(a.i, b.i);
+        return result.f;
+    }
+
+    float rpiLop_fpsub(float x, float y){
+        UNSAFEFPTHING a, b, result;
+        a.f = x;
+        b.f = y*-1;
+
+        result.i = rpiLop_fpadd2(a.i, b.i);
+        return result.f;
+    }
+
+    uint32_t rpiLop_fpmul2(uint32_t x, uint32_t y){
+        uint32_t result_mantissa;
+        uint32_t result_exponent;
+        uint32_t result_sign;
+
+        uint32_t different_sign = sign(x) ^ sign(y);
+
+        // catch NaN
+        if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
+        if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
+
+        // catch Inf
+        if (!(exponent(x) ^ 0xFF)) return x;
+        if (!(exponent(y) ^ 0xFF)) return y;
+
+        result_sign = sign(x) ^ sign(y);
+        result_exponent = exponent(x) + exponent(y) - 127; // subtract bias;
+        result_mantissa = mantissa(x)*mantissa(y);
+
+        //round;
         if (result_mantissa << 31)  result_mantissa = ((result_mantissa >> 1) + 1);
         else result_mantissa = (result_mantissa >> 1);
 
@@ -154,120 +257,65 @@ uint32_t rpiLop_fpadd2(uint32_t x, uint32_t y){
                 break;
             }
         }
+
+        uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
+        return result;
     }
-    uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
-    return result;
-}
 
-float rpiLop_fpadd(float x, float y){
-  UNSAFEFPTHING a, b, result;
-  a.f = x;
-  b.f = y;
 
-  result.i = rpiLop_fpadd2(a.i, b.i);
-  return result.f;
-}
 
-float rpiLop_fpsub(float x, float y){
-  UNSAFEFPTHING a, b, result;
-  a.f = x;
-  b.f = y*-1;
+    uint32_t rpiLop_fpdiv2(uint32_t x, uint32_t y){
+        uint32_t result_mantissa;
+        uint32_t result_exponent;
+        uint32_t result_sign;
 
-  result.i = rpiLop_fpadd2(a.i, b.i);
-  return result.f;
-}
+        uint32_t different_sign = sign(x) ^ sign(y);
 
-uint32_t rpiLop_fpmul2(uint32_t x, uint32_t y){
-    uint32_t result_mantissa;
-    uint32_t result_exponent;
-    uint32_t result_sign;
+        // catch NaN
+        if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
+        if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
 
-    uint32_t different_sign = sign(x) ^ sign(y);
+        // catch Inf
+        if (!(exponent(x) ^ 0xFF)) return x;
+        if (!(exponent(y) ^ 0xFF)) return y;
 
-    // catch NaN
-    if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
-    if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
+        result_sign = sign(x) ^ sign(y);
+        result_exponent = exponent(x) - exponent(y) + 127; // add bias;
+        result_mantissa = mantissa(x)/mantissa(y);
 
-    // catch Inf
-    if (!(exponent(x) ^ 0xFF)) return x;
-    if (!(exponent(y) ^ 0xFF)) return y;
+        //round;
+        if (result_mantissa << 31)  result_mantissa = ((result_mantissa >> 1) + 1);
+        else result_mantissa = (result_mantissa >> 1);
 
-    result_sign = sign(x) ^ sign(y);
-    result_exponent = exponent(x) + exponent(y) - 127; // subtract bias;
-    result_mantissa = mantissa(x)*mantissa(y);
-
-    //round;
-    if (result_mantissa << 31)  result_mantissa = ((result_mantissa >> 1) + 1);
-    else result_mantissa = (result_mantissa >> 1);
-
-    // normalize mantissa
-    uint32_t temp = result_mantissa << 9;
-    for (uint32_t count = 0; count < 23; ++count) {
-        if (!((temp << count) >> 31)) {
-            result_mantissa <<= ++count; // leading 1, so shift 1 more time
-            result_exponent -= count;
-            break;
+        // normalize mantissa
+        uint32_t temp = result_mantissa << 9;
+        for (uint32_t count = 0; count < 23; ++count) {
+            if (!((temp << count) >> 31)) {
+                result_mantissa <<= ++count; // leading 1, so shift 1 more time
+                result_exponent -= count;
+                break;
+            }
         }
+
+        uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
+        return result;
     }
 
-    uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
-    return result;
-}
+    float rpiLop_fpmul(float x, float y){
+        UNSAFEFPTHING a, b, result;
+        a.f = x;
+        b.f = y;
 
-
-
-uint32_t rpiLop_fpdiv2(uint32_t x, uint32_t y){
-    uint32_t result_mantissa;
-    uint32_t result_exponent;
-    uint32_t result_sign;
-
-    uint32_t different_sign = sign(x) ^ sign(y);
-
-    // catch NaN
-    if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
-    if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
-
-    // catch Inf
-    if (!(exponent(x) ^ 0xFF)) return x;
-    if (!(exponent(y) ^ 0xFF)) return y;
-
-    result_sign = sign(x) ^ sign(y);
-    result_exponent = exponent(x) - exponent(y) + 127; // add bias;
-    result_mantissa = mantissa(x)/mantissa(y);
-
-    //round;
-    if (result_mantissa << 31)  result_mantissa = ((result_mantissa >> 1) + 1);
-    else result_mantissa = (result_mantissa >> 1);
-
-    // normalize mantissa
-    uint32_t temp = result_mantissa << 9;
-    for (uint32_t count = 0; count < 23; ++count) {
-        if (!((temp << count) >> 31)) {
-            result_mantissa <<= ++count; // leading 1, so shift 1 more time
-            result_exponent -= count;
-            break;
-        }
+        result.i = rpiLop_fpmul2(a.i, b.i);
+        return result.f;
     }
 
-    uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
-    return result;
-}
+    float rpiLop_fpdiv(float x, float y){
+        UNSAFEFPTHING a, b, result;
+        a.f = x;
+        b.f = y;
 
-float rpiLop_fpmul(float x, float y){
-  UNSAFEFPTHING a, b, result;
-  a.f = x;
-  b.f = y;
-
-  result.i = rpiLop_fpmul2(a.i, b.i);
-  return result.f;
-}
-
-float rpiLop_fpdiv(float x, float y){
-  UNSAFEFPTHING a, b, result;
-  a.f = x;
-  b.f = y;
-
-  result.i = rpiLop_fpdiv2(a.i, b.i);
-  return result.f;
-}
+        result.i = rpiLop_fpdiv2(a.i, b.i);
+        return result.f;
+    }
 }
--- src/IReplace.cpp
+++ src/IReplace.cpp
@@ -15,97 +15,102 @@ namespace{
         unsigned Count;
         IReplaceVisitor() : Count(0) {}
 
+
         void visitBinaryOperator(BinaryOperator &I) {
             Count++;
-
             if (I.getOpcode() == Instruction::Add) {
+                auto fname = (I.getOperand(0)->getType() == Type::getInt64Ty(I.getContext()))?"rpiLop_add64":"rpiLop_add";
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualint(2, Type::getInt32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
+                std::vector<Type*> dualint(2, I.getOperand(0)->getType());
+                auto t = FunctionType::get(I.getOperand(0)->getType(),
                                            dualint,
                                            false);
-                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_add", t));
+                auto addFunc = cast<Function>(M->getOrInsertFunction(fname, t));
                 auto ci = CallInst::Create(t, addFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::Sub) {
+                auto fname = (I.getOperand(0)->getType() == Type::getInt64Ty(I.getContext()))?"rpiLop_sub64":"rpiLop_sub";
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualint(2, Type::getInt32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
+                std::vector<Type*> dualint(2, I.getOperand(0)->getType());
+                auto t = FunctionType::get(I.getOperand(0)->getType(),
                                            dualint,
                                            false);
-                auto subFunc = cast<Function>(M->getOrInsertFunction("rpiLop_subtract", t));
+                auto subFunc = cast<Function>(M->getOrInsertFunction(fname, t));
                 auto ci = CallInst::Create(t, subFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::Mul) {
+                auto fname = (I.getOperand(0)->getType() == Type::getInt64Ty(I.getContext()))?"rpiLop_mul64":"rpiLop_mul";
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualint(2, Type::getInt32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
+                std::vector<Type*> dualint(2, I.getOperand(0)->getType());
+                auto t = FunctionType::get(I.getOperand(0)->getType(),
                                            dualint,
                                            false);
-                auto mulFunc = cast<Function>(M->getOrInsertFunction("rpiLop_multiply", t));
+                auto mulFunc = cast<Function>(M->getOrInsertFunction(fname, t));
                 auto ci = CallInst::Create(t, mulFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::UDiv) {
+                auto fname = (I.getOperand(0)->getType() == Type::getInt64Ty(I.getContext()))?"rpiLop_div64":"rpiLop_div";
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualint(2, Type::getInt32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
+                std::vector<Type*> dualint(2, I.getOperand(0)->getType());
+                auto t = FunctionType::get(I.getOperand(0)->getType(),
                                            dualint,
                                            false);
-                auto divFunc = cast<Function>(M->getOrInsertFunction("rpiLop_divide", t));
+                auto divFunc = cast<Function>(M->getOrInsertFunction(fname, t));
                 auto ci = CallInst::Create(t, divFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
-            else if (I.getOpcode() == Instruction::SDiv) {
-                auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualint(2, Type::getInt32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getInt32Ty(I.getContext()),
-                                           dualint,
-                                           false);
-                auto sdivFunc = cast<Function>(M->getOrInsertFunction("rpiLop_divide", t));
-                auto ci = CallInst::Create(t, sdivFunc, {I.getOperand(0), I.getOperand(1)}, "");
-                ReplaceInstWithInst(&I, ci);
-            }
+            // else if (I.getOpcode() == Instruction::SDiv) {
+            //     auto fname = (I.getOperand(0)->getType() == Type::getInt64Ty(I.getContext()))?"rpiLop_div64":"rpiLop_div";
+            //     auto M = I.getParent()->getParent()->getParent();
+            //     std::vector<Type*> dualint(2, I.getOperand(0)->getType());
+            //     auto t = FunctionType::get(I.getOperand(0)->getType(),
+            //                                dualint,
+            //                                false);
+            //     auto sdivFunc = cast<Function>(M->getOrInsertFunction(fname, t));
+            //     auto ci = CallInst::Create(t, sdivFunc, {I.getOperand(0), I.getOperand(1)}, "");
+            //     ReplaceInstWithInst(&I, ci);
+            // }
             else if (I.getOpcode() == Instruction::FAdd) {
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                std::vector<Type*> dualfloat(2, Type::getFloatTy(I.getContext()));
+                auto t = FunctionType::get(Type::getFloatTy(I.getContext()),
                                            dualfloat,
                                            false);
-                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fadd", t));
+                auto addFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fpadd", t));
                 auto ci = CallInst::Create(t, addFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::FSub) {
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                std::vector<Type*> dualfloat(2, Type::getFloatTy(I.getContext()));
+                auto t = FunctionType::get(Type::getFloatTy(I.getContext()),
                                            dualfloat,
                                            false);
-                auto subFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fsub", t));
+                auto subFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fpsub", t));
                 auto ci = CallInst::Create(t, subFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::FMul) {
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                std::vector<Type*> dualfloat(2, Type::getFloatTy(I.getContext()));
+                auto t = FunctionType::get(Type::getFloatTy(I.getContext()),
                                            dualfloat,
                                            false);
-                auto mulFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fmul", t));
+                auto mulFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fpmul", t));
                 auto ci = CallInst::Create(t, mulFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
             else if (I.getOpcode() == Instruction::FDiv) {
                 auto M = I.getParent()->getParent()->getParent();
-                std::vector<Type*> dualfloat(2, Type::getFloat32Ty(I.getContext()));
-                auto t = FunctionType::get(Type::getFloat32Ty(I.getContext()),
+                std::vector<Type*> dualfloat(2, Type::getFloatTy(I.getContext()));
+                auto t = FunctionType::get(Type::getFloatTy(I.getContext()),
                                            dualfloat,
                                            false);
-                auto divFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fdiv", t));
-                auto ci = CallInst::Create(t, mulFunc, {I.getOperand(0), I.getOperand(1)}, "");
+                auto divFunc = cast<Function>(M->getOrInsertFunction("rpiLop_fpdiv", t));
+                auto ci = CallInst::Create(t, divFunc, {I.getOperand(0), I.getOperand(1)}, "");
                 ReplaceInstWithInst(&I, ci);
             }
         }
